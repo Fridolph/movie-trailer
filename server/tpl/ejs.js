c34277b3e917a52164b919ef81222b0eefc44c46
fridolph <249121486@qq.com>
2018-03-18 23:33:00 +0800

    项目初始化，相关配置文件

---
+++ server/tpl/ejs.js
@@ -0,0 +1,27 @@
+module.exports = `
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Koa Server Html</title>
+  <link href="https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <h1>Hi <%= you %></h1>
+        <p>This is <%= me %></p>
+      </div>
+      <div class="col-md-4">
+        <p>测试静态html页面</p>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://cdn.bootcss.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+`
